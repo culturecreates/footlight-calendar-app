d3a699aee3924c642b8c50d3f4fc1c1521c117c5
Raseem <raseem.ayatt@ust.com>
2022-06-22 18:36:17 +0530

    fixed recurring event ui issues

--- src/pages/AddEvent.js
+++ src/pages/AddEvent.js
@@ -223,12 +223,12 @@ const AddEvent = function ({ currentLang, eventDetails }) {
         setNumberOfDays(eventDetails.subEvents?.length)
         form.setFieldsValue({
           frequency: eventDetails.recurringEvent?.frequency,
-          startDateRecur: [moment(new Date(eventDetails.recurringEvent?.startDate), "DD-MM-YYYY").tz(eventDetails.scheduleTimezone?eventDetails.scheduleTimezone:"Canada/Eastern"),
-           moment(new Date(eventDetails.recurringEvent?.endDate), "DD-MM-YYYY").tz(eventDetails.scheduleTimezone?eventDetails.scheduleTimezone:"Canada/Eastern")],
+          startDateRecur: [moment(new Date(eventDetails.recurringEvent?.startDate), "DD-MM-YYYY"),
+           moment(new Date(eventDetails.recurringEvent?.endDate), "DD-MM-YYYY")],
           startTimeRecur: moment(eventDetails.recurringEvent?.startTime, "HH:mm"),
           endTimeRecur: moment(eventDetails.recurringEvent?.endTime, "HH:mm"),
           
-          daysOfWeek:eventDetails.recurringEvent?.days
+          daysOfWeek:eventDetails.recurringEvent?.weekDays
         })
         setIsRecurring(true)
       }
